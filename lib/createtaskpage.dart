1eb643a630fc05d2b809e87bb29d2fc05c2c7297
kioql <kioql2003@gmail.com>
2022-09-02 03:30:10 -0700

    Initial commit

---
+++ lib/createtaskpage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class taskadded extends StatefulWidget {
+  const taskadded({Key? key}) : super(key: key);
+
+  @override
+  State<taskadded> createState() => _taskaddedState();
+}
+
+class _taskaddedState extends State<taskadded> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Container(color: Colors.amber,));
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:todo/createtaskpage.dart';
+
+void main() {
+  runApp(MaterialApp(home: taskadded(),));
+}
